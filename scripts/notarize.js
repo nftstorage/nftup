9d9780353020da5f4305052818d67965472a82d3
Alan Shaw <alan.shaw@protocol.ai>
2022-04-01 11:33:37 +0100

    chore: configuration for MacOS code signing and notarization
    References:
    https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
    https://github.com/ipfs/ipfs-desktop/issues/66#issuecomment-456384158
    https://github.com/marketplace/actions/electron-builder-action#code-signing

---
+++ scripts/notarize.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const { notarize } = require('electron-notarize')
+
+exports.default = async function notarizing (context) {
+  const { electronPlatformName, appOutDir } = context
+  if (electronPlatformName !== 'darwin') return
+
+  if (!process.env.APPLEID || !process.env.APPLEIDPASS) {
+    return console.warn('skipping notarization: missing APPLEID and APPLEIDPASS environment variables')
+  }
+
+  const appName = context.packager.appInfo.productFilename
+
+  return await notarize({
+    appBundleId: 'storage.nft.nftup',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS
+  })
+}
