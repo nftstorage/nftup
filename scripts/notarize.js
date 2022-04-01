029bf4689dbd308cb5640c5c13b2dcfd037ffebd
Alan Shaw <alan.shaw@protocol.ai>
2022-04-01 11:48:56 +0100

    fix: add missing team ID

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -15,6 +15,7 @@ exports.default = async function notarizing (context) {
     appBundleId: 'storage.nft.nftup',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS
+    appleIdPassword: process.env.APPLEIDPASS,
+    teamId: process.env.TEAMID
   })
 }
