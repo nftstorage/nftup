9ee9034d5090f0b5a7d625ea254ae88f35f5ed48
Alan Shaw <alan.shaw@protocol.ai>
2022-04-08 15:35:05 +0100

    fix: pluralization of chunks

--- src/UploadProgress.js
+++ src/UploadProgress.js
@@ -7,7 +7,7 @@ export function UploadProgress ({ statusText, storedBytes, storedChunks, totalBy
   return (
     <div className='ma4 flex-auto flex items-center'>
       <div className='w-100 pa4 ba bg-white'>
-        <p className='f4 mv1'><strong>{fmt(totalFiles)}</strong> files, {storedBytes ? <span><strong>{bytes(storedBytes)}</strong><span> of </span></span> : ''}<strong>{bytes(totalBytes)}</strong>{storedChunks ? <span className='f6 fr mt1'>{fmt(storedChunks)} chunks</span> : ''}</p>
+        <p className='f4 mv1'><strong>{fmt(totalFiles)}</strong> files, {storedBytes ? <span><strong>{bytes(storedBytes)}</strong><span> of </span></span> : ''}<strong>{bytes(totalBytes)}</strong>{storedChunks ? <span className='f6 fr mt1'>{fmt(storedChunks)} chunk{storedChunks > 1 ? 's' : ''}</span> : ''}</p>
         {storedBytes
           ? <Meter max={totalBytes} value={storedBytes} />
           : <MeterIndeterminate />}
