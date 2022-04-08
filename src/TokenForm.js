f69ff9d60b9d0631585b3934272f28fa0d30b291
Chris Anderson <jchris@gmail.com>
2022-04-08 03:21:07 -0700

    feat: link to website for API key (#1)
    Tweaked so it looks like this now:
    
    <img width="992" alt="Screenshot 2022-04-08 at 11 01 14" src="https://user-images.githubusercontent.com/152863/162414131-05041455-fa2f-4e17-bad1-39d84dbfc3ae.png">

--- src/TokenForm.js
+++ src/TokenForm.js
@@ -8,7 +8,11 @@ export function TokenForm ({ onToken }) {
   return (
     <div className='ma4 flex-auto flex items-center'>
       <form className='w-100 pa4' onSubmit={handleSubmit}>
-        <textarea id='key' className='w-100 pa3 sans-serif mb2 ba b--black br1' rows='5' placeholder='Enter your NFT.Storage API key.' required spellcheck='false' onInput={handleInput} />
+        <textarea id='key' className='w-100 pa3 sans-serif db mb2 ba b--black br1' rows='5' placeholder='Enter your NFT.Storage API key.' required spellcheck='false' onInput={handleInput} />
+        <p className='f6 mt2 mb3'>
+          <img src='icon-question.svg' alt='question icon' className='v-mid mr1' style={{ width: 18 }} />
+          <span className='v-mid'>API keys can be found <a href='https://nft.storage/manage/' target='_blank' rel='noreferrer' className='black'>on your account page</a>.</span>
+        </p>
         <div className='tc'>
           <button type='submit' className='ph4 pv2 bg-green ba b--dark-green br1 light-green hover-white'>Continue</button>
         </div>
